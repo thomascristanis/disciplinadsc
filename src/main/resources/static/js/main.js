da2e435999890517119a2e0bc77c3040c81753e2
Thomas Cristanis <thomascristanis@gmail.com>
2019-10-24 20:34:54 -0300

    Atualização do main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,11 @@
+var tabela;
 $(document).ready( function () {
-    var table = $('#tabelaAlunos').DataTable({
+
+    tabela = $('#tabelaAlunos').DataTable({
         //   paging: false,
         //   searching: false,
           "language": {
+               "emptyTable": 'Nenhum resultado foi encontrado',
               "paginate": {
                   "previous": "Anterior",
                   "next": "Próximo"
@@ -11,11 +14,23 @@ $(document).ready( function () {
         "sAjaxSource": "/api/aluno/listar",
         "sAjaxDataProp": "",
         "order": [[ 0, "asc" ]],
+        "columnDefs": [
+            { "width": "10%", "targets": 0 }, //ID
+            { "width": "20%", "targets": 1 }, //matricula
+            { "width": "40%", "targets": 2 }, //nome
+            { "width": "10%", "targets": 3 }, //faltas
+            { "width": "20%", "targets": 4 } //editar/deletar
+        ],
         "aoColumns": [
             { "mData": "id"},
-            { "mData": "nome" },
             { "mData": "matricula" },
-            { "mData": "faltas" }
+            { "mData": "nome" },
+            { "mData": "faltas" },
+            {
+                data: null,
+                className: "center",
+                defaultContent: '<button type="button" class="editor_edit btn btn-warning btn-sm" data-toggle="modal" data-target="#modalEditar">Editar</button>  <button id="btnTableDeletar" type="button" class="editor_remove btn btn-danger btn-sm" data-toggle="modal" data-target="#modalDeletar">Deletar</button>'
+            }
         ]
-	 })
+     })
 });
