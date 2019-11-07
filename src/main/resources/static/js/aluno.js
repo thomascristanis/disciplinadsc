743fa027a2d4e579be7db9f62be062f698a95859
Thomas Cristanis <thomascristanis@gmail.com>
2019-11-07 14:48:18 -0300

    Melhoria na organização do js

--- src/main/resources/static/js/aluno.js
+++ src/main/resources/static/js/aluno.js
@@ -1,6 +1,36 @@
 var tabela;
-$(document).ready(function () {
 
+$(document).ready(function() {
+
+    carregarTabela();
+});
+
+
+$(window).on('load', function() {
+
+    $('#modalNovo').on('show.bs.modal', function() {
+        carregarCurso($('#inputNovoCurso'));
+    });
+
+    $("#btnSalvar").click(function() {
+        adicionarAluno();
+    });
+
+    $("#btnEditarSalvar").click(function() {
+        editarAluno();
+    });
+
+    $("#btnDeletar").click(function() {
+        deletarAluno();
+    });
+
+    $('#tabelaAlunos tbody').on('click', 'button', function() {
+        tratamentoClickButton(this);
+    });
+
+});
+
+function carregarTabela() {
     tabela = $('#tabelaAlunos').DataTable({
         //   paging: false,
         //   searching: false,
@@ -13,21 +43,49 @@ $(document).ready(function () {
         },
         "sAjaxSource": "/api/aluno/listar",
         "sAjaxDataProp": "",
-        "order": [[0, "asc"]],
-        "columnDefs": [
-            { "width": "5%", "targets": 0 }, //ID
-            { "width": "30%", "targets": 1 }, //ID
-            { "width": "10%", "targets": 2 }, //ID
-            { "width": "30%", "targets": 3 }, //ID
-            { "width": "10%", "targets": 4 }, //ID
-            { "width": "15%", "targets": 5 }, //ID
+        "order": [
+            [0, "asc"]
         ],
-        "aoColumns": [
-            { "mData": "id" },
-            { "mData": "matricula" },
-            { "mData": "nome" },
-            { "mData": "faltas" },
-            { "mData": "curso.nome" },
+        "columnDefs": [{
+                "width": "5%",
+                "targets": 0
+            }, //ID
+            {
+                "width": "15%",
+                "targets": 1
+            }, //Matricula
+            {
+                "width": "30%",
+                "targets": 2
+            }, //Nome
+            {
+                "width": "10%",
+                "targets": 3
+            }, //Faltas
+            {
+                "width": "25%",
+                "targets": 4
+            }, //Curso
+            {
+                "width": "15%",
+                "targets": 5
+            }, //Editar/Deletar
+        ],
+        "aoColumns": [{
+                "mData": "id"
+            },
+            {
+                "mData": "matricula"
+            },
+            {
+                "mData": "nome"
+            },
+            {
+                "mData": "faltas"
+            },
+            {
+                "mData": "curso.nome"
+            },
             {
                 data: null,
                 className: "center",
@@ -35,196 +93,144 @@ $(document).ready(function () {
             }
         ]
     });
+}
 
-    //Função para Adicionar
-    $("#btnSalvar").click(function () {
+function carregarCurso(inputCurso) {
 
-        var vData = JSON.stringify({
-            "matricula": $('#inputNovoMatricula').val(),
-            "nome": $('#inputNovoNome').val(),
-            "faltas": $('#inputNovoFaltas').val(),
-            "curso": {
-                id: $('#inputNovoCurso option:selected').val(),
-                nome: $('#inputNovoCurso option:selected').text()
+    var optionCount = $(inputCurso).find("option").length
+
+    if (optionCount == 1) { //Elemento Escolher
+        $.ajax({
+            url: "/api/curso/listar",
+            type: "GET",
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: function(obj) {
+                if (obj != null) {
+                    var data = obj;
+                    // var selectbox = $('#inputNovoCurso');
+                    $.each(data, function(i, d) {
+                        inputCurso.append(new Option(d.nome, d.id));
+                    });
+                }
             }
         });
+    }
+}
 
-        $.ajax({
+function adicionarAluno() {
+    var vData = JSON.stringify({
+        "matricula": $('#inputNovoMatricula').val(),
+        "nome": $('#inputNovoNome').val(),
+        "faltas": $('#inputNovoFaltas').val(),
+        "curso": {
+            id: $('#inputNovoCurso option:selected').val(),
+            nome: $('#inputNovoCurso option:selected').text()
+        }
+    });
+
+    $.ajax({
             url: "/api/aluno/salvar",
             type: "POST",
             contentType: "application/json;charset=UTF-8",
             dataType: "json",
             data: vData,
-            beforeSend: function () {
-                $("#resultado").html("ENVIANDO...");
+            beforeSend: function() {
+
             }
         })
-            .done(function (msg) {
-                console.log("Aluno salvo!");
-                $('#modalNovo').modal('hide');
-                tabela.ajax.reload();
-            })
-            .fail(function (jqXHR, textStatus, msg) {
-                alert("ERRO " + msg);
-            });
-    });
-
-    //Carregar Aluno
-    $('#modalNovo').on('show.bs.modal', function () {
-        //Desabilita a opção Escolha...
-        $("#inputNovoCurso option:selected").attr('disabled', 'disabled');
-
-        var optionCount = $('#inputNovoCurso option').length;
-
-        if (optionCount == 1) { //Elemento Escolher
-            $.ajax({
-                url: "/api/curso/listar",
-                type: "GET",
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: function (obj) {
-                    if (obj != null) {
-                        var data = obj;
-                        var selectbox = $('#inputNovoCurso');
-                        $.each(data, function (i, d) {
-                            selectbox.append(new Option(d.nome, d.id));
-                        });
-                    }
-                }
-            });
-        }
-    })
-
-    $('#modalEditar').on('show.bs.modal', function () {
-        //Desabilita a opção Escolha...
-        $("#inputEditarCurso option:selected").attr('disabled', 'disabled');
-
-        var optionCount = $('#inputEditarCurso option').length;
-
-        if (optionCount == 1) { //Elemento Escolher
-            $.ajax({
-                url: "/api/curso/listar",
-                type: "GET",
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: function (obj) {
-                    if (obj != null) {
-                        var data = obj;
-                        var selectbox = $('#inputEditarCurso');
-                        $.each(data, function (i, d) {
-                            // selectbox.append(new Option(d.nome, d.id));
-                            // $('<option>').val(JSON.stringify(d)).text(d.nome).appendTo(selectbox);
-                        });
-                    }
-                }
-            });
-        }
-    })
-
-    //Função para Editar Aluno
-    $("#btnEditarSalvar").click(function () {
-        var vData = JSON.stringify({
-            "matricula": $('#inputEditarMatricula').val(),
-            "nome": $('#inputEditarNome').val(),
-            "faltas": $('#inputEditarFaltas').val(),
-            "curso": {
-                id: $('#inputEditarCurso option:selected').val(),
-                nome: $('#inputEditarCurso option:selected').text()
-            }
+        .done(function(msg) {
+            console.log("Aluno salvo!");
+            $('#modalNovo').modal('hide');
+            tabela.ajax.reload();
+        })
+        .fail(function(jqXHR, textStatus, msg) {
+            alert("ERRO " + msg);
         });
+}
 
-        $.ajax({
+function editarAluno() {
+    var vData = JSON.stringify({
+        "matricula": $('#inputEditarMatricula').val(),
+        "nome": $('#inputEditarNome').val(),
+        "faltas": $('#inputEditarFaltas').val(),
+        "curso": {
+            id: $('#inputEditarCurso option:selected').val(),
+            nome: $('#inputEditarCurso option:selected').text()
+        }
+    });
+    $.ajax({
             url: "/api/aluno/editar/" + $('#fieldEditarID').val(),
             type: "PUT",
             contentType: "application/json;charset=UTF-8",
             dataType: "json",
             data: vData,
         })
-            .done(function (msg) {
-                console.log("Aluno editado!");
-                $('#modalEditar').modal('hide');
-                tabela.ajax.reload();
-            })
-            .fail(function (jqXHR, textStatus, msg) {
-                alert("ERRO " + msg);
-            });
-    });
+        .done(function(msg) {
+            console.log("Aluno editado!");
+            $('#modalEditar').modal('hide');
+            tabela.ajax.reload();
+        })
+        .fail(function(jqXHR, textStatus, msg) {
+            alert("ERRO " + msg);
+        });
 
-    //Deletar Aluno
-    $("#btnDeletar").click(function () {
+}
 
-        $.ajax({
+function deletarAluno() {
+
+    var dele = $('#fieldDeleteID').val();
+
+    $.ajax({
             url: "/api/aluno/deletar/" + $('#fieldDeleteID').val(),
             type: "DELETE",
         })
-            .done(function (msg) {
-                console.log("Aluno deletado!");
-                $('#modalDeletar').modal('hide');
-                tabela.ajax.reload();
-            })
-            .fail(function (jqXHR, textStatus, msg) {
-                alert("ERRO " + msg);
-            });
-    });
+        .done(function(msg) {
+            console.log("Aluno deletado!");
+            $('#modalDeletar').modal('hide');
+            tabela.ajax.reload();
+        })
+        .fail(function(jqXHR, textStatus, msg) {
+            alert("ERRO " + msg);
+        });
+}
 
-    $('#tabelaAlunos tbody').on('click', 'button', function () {
+function tratamentoClickButton(button) {
 
-        //Carrega o Curso
+    var linha = $(button).parent().parent();
+    var id = tabela.row(linha).data().id;
 
-        $("#inputEditarCurso option:selected").attr('disabled', 'disabled');
+    if (button.innerText == "Editar") {
 
-        var optionCount = $('#inputEditarCurso option').length;
+        carregarCurso($('#inputEditarCurso'));
 
-        if (optionCount == 1) { //Elemento Escolher
-            $.ajax({
-                url: "/api/curso/listar",
-                type: "GET",
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: function (obj) {
-                    if (obj != null) {
-                        var data = obj;
-                        var selectbox = $('#inputEditarCurso');
-                        $.each(data, function (i, d) {
-                            selectbox.append(new Option(d.nome, d.id));
-                        });
-                    }
-                }
-            });
-        }
+        $("#fieldEditarID").val(id);
 
-        var linha = $(this).parent().parent();
-        var id = tabela.row(linha).data().id;
+        $.ajax({
+            url: "/api/aluno/listar/" + id,
+            type: "GET",
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: function(obj) {
+                if (obj != null) {
+                    $('#inputEditarMatricula').val(obj.matricula)
+                    $('#inputEditarNome').val(obj.nome)
+                    $('#inputEditarFaltas').val(obj.faltas)
 
-        if (this.innerText == "Editar") {
-            $("#fieldEditarID").val(id);
+                    var selectbox = $('#inputEditarCurso option');
 
-            $.ajax({
-                url: "/api/aluno/listar/" + id,
-                type: "GET",
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: function (obj) {
-                    if (obj != null) {
-                        $('#inputEditarMatricula').val(obj.matricula)
-                        $('#inputEditarNome').val(obj.nome)
-                        $('#inputEditarFaltas').val(obj.faltas)
+                    $.each(selectbox, function(i, d) {
+                        if (parseInt(d.value) == obj.curso.id) {
 
-                        var selectbox = $('#inputEditarCurso option');
+                            $(d).attr('selected', 'selected');
+                        }
+                    });
+                };
+            }
+        });
 
-                        $.each(selectbox, function (i, d) {
-                            if(d.value == obj.curso.id){
+    } else if (button.innerText == "Deletar") {
+        $("#fieldDeleteID").val(id);
 
-                                $(d).attr('selected', 'selected');
-                            }
-                        });
-                    };
-                }
-            });
-
-        } else if (this.innerText == "Deletar") {
-            $("#fieldDeleteID").val(id);
-
-        }
-    });
-});
+    }
+}
